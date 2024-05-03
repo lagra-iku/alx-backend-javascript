704b10e079a142a1602fa502f0f845c8852ce26b
Grace O. Nweke <lagra2001@gmail.com>
2024-05-03 21:40:39 +0000

    sp;ving for task 8

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,30 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Index page', function() {
+    let server;
+
+    before(function() {
+        server = require('./api');
+    });
+
+    after(function() {
+        server.close();
+    });
+
+    it('should return correct status code', function(done) {
+        request.get('http://localhost:7865', function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+
+    it('should return correct result', function(done) {
+        request.get('http://localhost:7865', function(error, response, body) {
+            expect(body).to.equal('Welcome to the payment system');
+            done();
+        });
+    });
+
+});
+
