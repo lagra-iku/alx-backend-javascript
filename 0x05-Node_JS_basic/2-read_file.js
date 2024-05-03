e159d57b1d0dbce27b355dc2a8b978139fcd8982
Grace O. Nweke <lagra2001@gmail.com>
2024-05-03 14:09:59 +0000

    solving task 3

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,28 +1,29 @@
 const fs = require('fs');
 
 function countStudents(path) {
-    try {
-        const data = fs.readFileSync(path, 'utf8');
-        const lines = data.split('\n').filter(line => line.trim() !== '');
-        const header = lines.shift();
-        const counters = {};
-        lines.forEach(line => {
-            const [firstname, lastname, age, field] = line.split(',');
-            if (!counters[field]) {
-                counters[field] = [];
-            }
-            counters[field].push(firstname);
-        });
-        const totalStudents = lines.length;
-        console.log(`Number of students: ${totalStudents}`);
-        for (const field in counters) {
-            const studentsInField = counters[field].length;
-            const studentList = counters[field].join(', ');
-            console.log(`Number of students in ${field}: ${studentsInField}. List: ${studentList}`);
-        }
-    } catch (error) {
-        throw new Error('Cannot load the database');
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const counters = {};
+    lines.forEach((line) => {
+      const [firstname, , , field] = line.split(',');
+      if (field) {
+        counters[field] = counters[field] || [];
+        counters[field].push(firstname);
+      }
+    });
+    const totalStudents = lines.length;
+    console.log(`Number of students: ${totalStudents}`);
+    for (const field in counters) {
+      if (counters.hasOwnProperty(field)) {
+        const studentsInField = counters[field].length;
+        const studentList = counters[field].join(', ');
+        console.log(`Number of students in ${field}: ${studentsInField}. List: ${studentList}`);
+      }
     }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
